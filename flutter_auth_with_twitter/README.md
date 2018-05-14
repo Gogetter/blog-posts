9b0c8364ee3165e0197440882f3e58fc7760ef3b
etimbukudofia <euedofia@gmail.com>
2018-05-14 06:30:01 +0100

    initial commit

---
+++ flutter_auth_with_twitter/README.md
@@ -0,0 +1,8 @@
+# flutter_auth_with_twitter
+
+Flutter+Firebase - An example authenticating with Twitter
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ flutter_auth_with_twitter/lib/util/strings.dart
@@ -0,0 +1,6 @@
+/// strings class following from strings.xml
+class Strings {
+  static final String twitterApiKey = 'pjlQim7zBwt5Ncx2WGh9MVoBE';
+  static final String twitterApiSecret = 'jpK1dPJT6Ln9eg5YrIXa8Adh2ePI6jbNM5IHppIyepVg8EV5vh';
+  static final String twitterText = 'Sign In with Twitter';
+}
