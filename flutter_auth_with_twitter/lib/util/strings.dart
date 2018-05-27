dda88803856b3e00c429ed317e603320bcfbad85
etimbukudofia <euedofia@gmail.com>
2018-05-27 14:23:07 +0100

    added more string constants

--- flutter_auth_with_twitter/lib/util/strings.dart
+++ flutter_auth_with_twitter/lib/util/strings.dart
@@ -3,4 +3,6 @@ class Strings {
   static final String twitterApiKey = 'YOUR_TWITTER_API_KEY';
   static final String twitterApiSecret = 'YOUR_TWITTER_API_SECRET';
   static final String twitterText = 'Sign In with Twitter';
+  static final String appName = 'Flutter Journey';
+  static final String home = 'Home';
 }
