5a37e2f94ba7340d3a40b24689ecf9759d6744cf
etimbukudofia <euedofia@gmail.com>
2018-05-27 14:21:00 +0100

    drawer impl

---
+++ flutter_auth_with_twitter/lib/drawer/my_drawer_home_page.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_auth_with_twitter/util/strings.dart';
+
+class MyDrawerHomePage extends StatefulWidget {
+  final FirebaseUser currentUser;
+
+  MyDrawerHomePage(this.currentUser);
+
+  @override
+  _MyDrawerHomePageState createState() => new _MyDrawerHomePageState(currentUser);
+}
+
+class _MyDrawerHomePageState extends State<MyDrawerHomePage> {
+  final FirebaseUser currentUser;
+
+  _MyDrawerHomePageState(this.currentUser);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle _drawerItemTxtStyle = new TextStyle(
+        fontSize: 14.5,
+        fontWeight: FontWeight.normal
+    );
+
+    final TextStyle _userInfoTxtStyle = new TextStyle(
+      color: Colors.black
+    );
+
+    return new Scaffold(
+
+      appBar: new AppBar(
+          centerTitle: true,
+          backgroundColor: const Color.fromRGBO(61, 167, 219, 0.85),
+          title: new Text(
+            Strings.appName,
+            style: new TextStyle(color: Colors.white),
+          )
+      ),
+
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+
+            //display current user info
+            new UserAccountsDrawerHeader(
+                decoration: new BoxDecoration(color: Colors.white),
+                currentAccountPicture:  new CircleAvatar(
+                    backgroundImage: currentUser.photoUrl.isNotEmpty ? 
+                    new NetworkImage(currentUser.photoUrl) : currentUser.displayName.substring(0)),
+                accountName: new Text('Welcome, ${currentUser.displayName}', style: _userInfoTxtStyle),
+                accountEmail: new Text(currentUser.email?? '', style:  _userInfoTxtStyle)
+            ),
+
+            //Drawer items
+            new ListTile(
+              leading: new Icon(Icons.home),
+              title: new Text(Strings.home, style: _drawerItemTxtStyle),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+
+            new Divider(color: Colors.blueGrey[200]),
+
+            new ListTile(
+              leading: new Icon(Icons.info),
+              title: new Text(Strings.home, style: _drawerItemTxtStyle),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
