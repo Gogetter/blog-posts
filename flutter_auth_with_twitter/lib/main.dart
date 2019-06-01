bea1922c1496411c3ef5a8dfdb9c0b9f3af8c23d
eau <euedofia@gmail.com>
2019-06-01 03:54:07 +0100

    removed unneeded comments

--- flutter_auth_with_twitter/lib/main.dart
+++ flutter_auth_with_twitter/lib/main.dart
@@ -10,20 +10,13 @@ void main() => runApp(new MyApp());
 final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(title: 'Flutter Auth With Twitter'),
@@ -33,15 +26,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -68,9 +52,9 @@ class _MyHomePageState extends State<MyHomePage> {
         consumerSecret: Strings.twitterApiSecret
     );
 
-   _twitterLoginResult = await twitterLogin.authorize();
-   _currentUserTwitterSession = _twitterLoginResult.session;
-   _twitterLoginStatus = _twitterLoginResult.status;
+    _twitterLoginResult = await twitterLogin.authorize();
+    _currentUserTwitterSession = _twitterLoginResult.session;
+    _twitterLoginStatus = _twitterLoginResult.status;
 
     switch (_twitterLoginStatus) {
       case TwitterLoginStatus.loggedIn:
@@ -96,11 +80,11 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+
+
+
+
+
       if (_twitterLoginStatus == TwitterLoginStatus.loggedIn && _currentUser != null) {
         _loggedInMessage = '$snackBarMessage ${_currentUser.displayName}';
 
@@ -113,39 +97,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget buildHome(BuildContext context) {
     return new Center(
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          new Container(
-            padding: const EdgeInsets.only(
-                top: 64.0, bottom: 64.0, left: 16.0, right: 16.0),
-            child: new RaisedButton(
-                color: const Color.fromRGBO(29, 161, 242, 1.0),
-                child: new Row(
-                  children: <Widget>[
-                    new Container(
-                        padding: const EdgeInsets.only(
-                            top: 8.0, bottom: 8.0, left: 16.0, right: 32.0),
-                        child: new Image.asset('assets/images/twitter.png', height: 25.0, width: 25.0,)),
-                    new Expanded(
-                      child: new Text(
-                        Strings.twitterText,
-                        style: new TextStyle(color: Colors.white),
-                      ),
-                    )
-                  ],
-                ),
-                onPressed: _handleTwitterSignIn
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new Container(
+              padding: const EdgeInsets.only(
+                  top: 64.0, bottom: 64.0, left: 16.0, right: 16.0),
+              child: new RaisedButton(
+                  color: const Color.fromRGBO(29, 161, 242, 1.0),
+                  child: new Row(
+                    children: <Widget>[
+                      new Container(
+                          padding: const EdgeInsets.only(
+                              top: 8.0, bottom: 8.0, left: 16.0, right: 32.0),
+                          child: new Image.asset('assets/images/twitter.png', height: 25.0, width: 25.0,)),
+                      new Expanded(
+                        child: new Text(
+                          Strings.twitterText,
+                          style: new TextStyle(color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                  onPressed: _handleTwitterSignIn
+              ),
             ),
-          ),
-          new Expanded(
-            child: new Text(
-              _loggedInMessage?? 'Not logged In...',
-              style: new TextStyle(color: Colors.black),
-            ),
-          )
-        ],
-      )
+            new Expanded(
+              child: new Text(
+                _loggedInMessage?? 'Not logged In...',
+                style: new TextStyle(color: Colors.black),
+              ),
+            )
+          ],
+        )
     );
   }
 
@@ -174,16 +158,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+
+
+
+
+
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
+
         title: new Text(widget.title),
       ),
       body: buildHome(context),
